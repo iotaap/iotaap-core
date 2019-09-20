ba31de38c7d15f9572b1e9da000a331e20a3749d
Ivan Golubic <ivan@mvt-solutions.com>
2019-09-20 19:20:26 +0200

    Added IoTaaP-Serial example

---
+++ examples/IoTaaP-Serial.cpp
@@ -0,0 +1,21 @@
+#include "IoTaaP.h"
+
+IoTaaP iotaap;
+
+void setup()
+{
+  // Initialize IoTaaP serial port with 115200 baud rate  
+  iotaap.serial.init(115200);
+  // Print line to the serial port
+  iotaap.serial.printLn("IoTaaP Serial Communication");
+  // Delay for 1 second
+  delay(1000);
+}
+
+void loop()
+{
+  // If character is received, send it back
+  if(iotaap.serial.isAvailable()){
+    iotaap.serial.writeChar(iotaap.serial.readChar());
+  }
+}
