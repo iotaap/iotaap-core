73c908d5c182180c58ae71fd49df93e6b4824251
Ivan Golubic <ivan@mvt-solutions.com>
2019-09-20 13:49:32 +0200

    Added IoTaaP-OLED example

---
+++ examples/IoTaaP-OLED.cpp
@@ -0,0 +1,25 @@
+#include "IoTaaP.h"
+
+IoTaaP iotaap;
+
+void setup()
+{
+  // Initialize OLED
+  iotaap.oled.init();
+  // Show some text
+  iotaap.oled.showText("Countdown", 40, 30);
+  delay(1500);
+}
+
+void loop()
+{
+
+  for (int i = 10; i >= 0; i--)
+  {
+    iotaap.oled.showText(String(i), 60, 32);
+    delay(500);
+  }
+  // Display IoTaaP logo
+  iotaap.oled.displayBitmap(0, 0, iotaap_splash_128x64, 1);
+  while (1);
+}
