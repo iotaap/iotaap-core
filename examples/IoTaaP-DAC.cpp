cb9905c85bdab43f04292e14d172e22731dcaaf8
Ivan Golubic <ivan@mvt-solutions.com>
2019-09-20 21:15:48 +0200

    Added IoTaaP-DAC example

---
+++ examples/IoTaaP-DAC.cpp
@@ -0,0 +1,22 @@
+#include "IoTaaP.h"
+
+IoTaaP iotaap;
+
+void setup()
+{
+}
+
+void loop()
+{
+  for (int i = 0; i <= 255; i++)
+  {
+    // Set DAC value
+    iotaap.dac.set(i);
+    delay(10);
+  }
+  for (int i = 255; i >= 0; i--)
+  {
+    iotaap.dac.set(i);
+    delay(10);
+  }
+}
