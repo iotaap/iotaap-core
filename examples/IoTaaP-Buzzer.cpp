9d25f3cc61f7b57beee2e16089099b5902bfa87a
Ivan Golubic <ivan@mvt-solutions.com>
2019-09-21 14:12:56 +0200

    Added IoTaaP-Buzzer example

---
+++ examples/IoTaaP-Buzzer.cpp
@@ -0,0 +1,22 @@
+#include "IoTaaP.h"
+
+IoTaaP iotaap;
+
+void setup()
+{
+  // Initialize buzzer
+  iotaap.buzzer.initBuzzer();
+  // Generate tone with 2400Hz frequency that will last for 300mS
+  iotaap.buzzer.generateTone(2400, false, 300);
+}
+
+void loop()
+{
+  for (int i = 0; i <= 20000; i = i + 10)
+  {
+    iotaap.buzzer.initBuzzer();
+    iotaap.buzzer.generateTone(i, false, 10);
+  }
+  // Detach buzzer
+  iotaap.buzzer.stopTone();
+}
