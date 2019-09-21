58c2666bf5f6b7fbb4ff2bb3d5f78fa4dc21b841
Ivan Golubic <ivan@mvt-solutions.com>
2019-09-21 14:41:06 +0200

    Added IoTaaP-ADC example

---
+++ examples/IoTaaP-ADC.cpp
@@ -0,0 +1,19 @@
+#include "IoTaaP.h"
+
+IoTaaP iotaap;
+
+void setup()
+{
+  iotaap.oled.init();
+}
+
+void loop()
+{
+  // Get RAW value from pin 26
+  uint16_t value = iotaap.adc.getValue(26);
+  // Convert RAW value to voltage
+  double voltage = iotaap.adc.getVoltage(value);
+  // Show data on the OLED
+  iotaap.oled.showText("RAW: " + String(value) + "\nVoltage: " + String(voltage));
+  delay(100);
+}
